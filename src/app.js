37c290e5dd93feb63a18e996cb934086fbbb75a7
mhhmzGitHub <mhhmz0@gmail.com>
2020-02-06 13:59:38 +0200

    Basic router Structure + user/auth

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const app = express()
 import {databaseConnect} from './database'
 import config from './config'
+import { router } from './router'
 
 databaseConnect()
 
-app.get('/', (req, res) => res.send('Welcome to Express'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/', router)
 
 const port = config.port
 
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import express from 'express'
+export const router = express.Router()
+import { userRouter } from './resources/users/user.router'
+
+router.get('/', (req, res) => res.send('Hello from router.js'))
+router.get('/about',(req, res) => res.send('About Page'))
+
+router.get('/signin', (req, res) => res.send('Sign In'))
+router.post('/signup', (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    const user = {email, password}
+    res.send(user)
+})
+router.use('/users', userRouter)
